89661da3aadac6d99059d4a56eddd7daacdb74f5
GuilhermeLeodeOliveira <97077427+GuilhermeLeodeOliveira@users.noreply.github.com>
2022-09-05 03:04:03 -0300

    teste

--- public/service-worker-to-install.js
+++ public/service-worker-to-install.js
@@ -1,13 +1,7 @@
 self.addEventListener('install', event => {
     console.log('sw ./ => installing...');
 
-    // cache a cat SVG
-    event.waitUntil(
-        caches.open('static-v1').then(cache => cache.add(''))
-    );
-    
-    console.log("sw ./ =>  install event detected e cat.svg cacheado!!!");
-    
+   
 });
 
 
@@ -22,11 +16,6 @@ self.addEventListener('fetch', event => {
     
     const url = new URL(event.request.url);
     
-    // serve the cat SVG from the cache if the request is
-    // same-origin and the path is '/dog.svg'
-    if (url.origin == location.origin && url.pathname == '/dog.svg') {
-        event. respondWith(caches.match('/cat.svg'));
-    }
 });
 
 
